1c41d1b1301a67d83d50b23d481cba6c0e320db5
ritheek-pitti <63711416+ritheek-pitti@users.noreply.github.com>
2021-07-17 21:35:56 +0530

    Reschedule is working

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         },
         problem:{
             required : true,
-            minlength : 15,
+            minlength : 1,
         },
         dateAndTime:{
             required : true,
@@ -27,4 +27,100 @@ $(document).ready(function() {
         }
       }
     });
+    $("#modalformid").validate({
+        rules: {
+          name : {
+            required: true,
+            minlength: 3
+          },
+          phone:{
+              required :true,
+              digits :true,
+          },
+          problem:{
+              required : true,
+              minlength : 1,
+          },
+          dateAndTime:{
+              required : true,
+          }
+          
+        },
+        messages : {
+          name: {
+            minlength: "Name should be at least 3 characters"
+          },
+          problem:{
+              minlength: "Please elaborate your problem"
+          }
+        }
+      });
+      let name,pNo,problem,dateAndTime,gender;
+    const formDetails = document.getElementById("form");
+    formDetails.addEventListener("submit", async (ev) => {
+       ev.preventDefault();
+       //console.log(formDetails);
+        name = document.getElementById("name").value;
+        pNo = document.getElementById("pNo").value;
+        problem = document.getElementById("problem").value;
+        dateAndTime = document.getElementById("dAndT").value;
+        let temp = document.getElementsByName("Gender");
+    for(i =0;i<temp.length;i++){
+      if(temp[i].checked){
+          console.log(temp[i].value);
+        gender = temp[i].value;
+      }
+    }
+       if( $("#form").valid())
+            {
+                $("#appointmentConfirmation").prop("hidden", false);
+                $("#nameDisplay").text("Name : "+name);
+                $("#numDisplay").text("Phone Number : "+pNo);
+                $("#descDisplay").text("Problem :"+problem);
+                $("#dateDisplay").text("Appointment Slot : "+dateAndTime);
+                $("#genderDisplay").text("Gender : "+gender);
+            }
+
+    })
+    $('#update').on('click', function(e) {
+        e.preventDefault();
+        console.log(gender);
+        $('#modname').val(name);
+        $('#modnum').val(pNo);
+        $('#moddesc').val(problem);
+        $('#moddate').val(dateAndTime);
+        if(gender==="Male"){
+          $('#modaldot-1').prop('checked', true);
+        }
+        else if(gender==="Female"){
+          $('#modaldot-2').prop('checked', true);
+        }
+      });
+      $('#modalsubmit').on('click', function(e) {
+        e.preventDefault();
+        //console.log(e.currentTarget.form[4].value);
+        name=e.currentTarget.form[0].value;
+        pNo= e.currentTarget.form[1].value;
+        problem= e.currentTarget.form[4].value;
+        dateAndTime= e.currentTarget.form[5].value;
+        gender='';
+        if(e.currentTarget.form[2].checked==true){
+          gender=e.currentTarget.form[2].value;
+        }
+        else if(e.currentTarget.form[3].checked==true){
+          gender=e.currentTarget.form[3].value;
+        }
+        //console.log(gender);
+        console.log($("#modalformid").valid());
+        if($("#modalformid").valid()){
+            //  $("#formid").hide();
+            $("#appointmentConfirmation").prop("hidden", false);
+            $("#nameDisplay").text("Name : "+name);
+            $("#numDisplay").text("Phone Number : "+pNo);
+            $("#descDisplay").text("Problem Description : \n"+problem);
+            $("#dateDisplay").text("Appointment Slot : "+dateAndTime);
+            $("#genderDisplay").text("Appointment Slot : "+gender);
+            $('#exampleModal').modal('toggle');
+        };
+      });
   });
